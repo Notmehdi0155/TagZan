c3a0e6e24a201a00acea2876532fab30297d3e07
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-14 21:14:22 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# توکن ربات
+BOT_TOKEN = '7585562804:AAH2NoU2SOPaRaQJBDnVGaLm-0UIot_dC7c'
+BOT_USERNAME = 'Davat_HotBot'
+
+# کانال‌هایی که عضویت اجباری دارن
+REQUIRED_CHANNELS = ['@hottof', '@Hottspots']
+
+# آیدی عددی ادمین
+ADMIN_ID = 6387942633
+
+# لینک سرور برای پینگ (برای جلوگیری از خاموشی Render)
+PING_URL = 'https://sim-dtlp.onrender.com/'
+
+# مدت زمان چک لفت دادن بر حسب ثانیه
+PING_INTERVAL = 240
