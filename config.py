2bf5ac1be313038ac9a0b7a70b325492f2ec7657
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-15 16:07:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "7301497411:AAHjTlpsh8wGUY-HNzrpZ6ecN8_qjC7qcRk"
-BOT_USERNAME = Up_jozve_bot
+BOT_USERNAME = "Up_jozve_bot"
 
 # آیدی عددی ادمین‌ها (میتونه چندتا باشه)
 ADMIN_IDS = [6387942633, 987654321]
@@ -11,4 +11,4 @@ MAIN_FORCE_SUB_CHANNEL = "https://t.me/+lkA4Yzgv3-g3MmY0"
 STORAGE_CHANNEL = "https://t.me/+IaDm18ikQ8g2Mjc8"
 
 # لینک کامل سرور رندر
-WEBHOOK_URL = "https://your-app-name.onrender.com"
+WEBHOOK_URL = "https://tagzan.onrender.com"
