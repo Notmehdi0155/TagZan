71025c24b6de31fecb74087295aabdf575e98480
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-15 16:01:34 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TOKEN = "توکن رباتت"
-BOT_USERNAME = "ربات_توبدون_@"  # مثل: "my_uploader_bot"
+TOKEN = "7301497411:AAHjTlpsh8wGUY-HNzrpZ6ecN8_qjC7qcRk"
+BOT_USERNAME = Up_jozve_bot
 
 # آیدی عددی ادمین‌ها (میتونه چندتا باشه)
-ADMIN_IDS = [123456789, 987654321]
+ADMIN_IDS = [6387942633, 987654321]
 
 # عضویت اجباری اصلی (ثابت)
 MAIN_FORCE_SUB_CHANNEL = "@channel_username"
