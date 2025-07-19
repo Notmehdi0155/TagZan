9474eaf30964a4c30b8c1601f1acecbd6fad4818
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-20 02:57:16 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = "7413532622:AAFok6PtchNhRIAFKn4q-Yu49PQ3cpaMGRU"
+BOT_TOKEN = "7301497411:AAGMxBMw8Fw3RYEqHU-S02qRJozJQ4qelic"
 WEBHOOK_URL = "https://tttttt-v1kw.onrender.com/webhook"
 ADMIN_IDS = [6387942633, 7189616405, 5459406429, 7143747068]  # شناسه عددی ادمین‌ها
 CHANNEL_TAG = "🔥@hottof | تُفِ داغ " 
