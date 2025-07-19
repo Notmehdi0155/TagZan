e13db9ac924ee53e61ddb0d73f6b85726c08ebb9
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-20 03:14:28 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,14 +3,21 @@ import string
 from database import save_file as db_save_file, get_file as db_get_file
 
 def gen_code(length=8):
-    """تابعی برای تولید یک کد تصادفی از حروف و اعداد"""
+    """
+    تولید یک کد تصادفی شامل حروف و اعداد (مثلاً برای لینک دعوت)
+    """
     characters = string.ascii_letters + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
     return code
 
 def save_file(file_id, code):
-    return db_save_file(file_id, code)
+    """
+    ذخیره فایل با استفاده از database.py
+    """
+    db_save_file(file_id, code)
 
 def get_file(code):
-    """دریافت فایل از دیتابیس بر اساس کد"""
+    """
+    دریافت فایل با استفاده از database.py
+    """
     return db_get_file(code)
