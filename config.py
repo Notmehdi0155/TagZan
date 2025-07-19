ef8bfacfc624b71f19beabed864ce39d1fbac9ea
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-20 02:53:39 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+BOT_TOKEN = "7413532622:AAFok6PtchNhRIAFKn4q-Yu49PQ3cpaMGRU"
+WEBHOOK_URL = "https://tttttt-v1kw.onrender.com/webhook"
+ADMIN_IDS = [6387942633, 7189616405, 5459406429, 7143747068]  # شناسه عددی ادمین‌ها
+CHANNEL_TAG = "🔥@hottof | تُفِ داغ " 
+PING_INTERVAL = 240  # فاصله زمانی بین پینگ‌ها به ثانیه
