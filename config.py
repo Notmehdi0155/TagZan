1b7899d33ee178bb8c8c4be37045f07abf0ad733
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-20 06:21:01 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+BOT_TOKEN = "7301497411:AAGMxBMw8Fw3RYEqHU-S02qRJozJQ4qelic"
+WEBHOOK_URL = "https://tagzan-1.onrender.com"
+ADMIN_IDS = [6387942633, 7189616405, 5459406429, 7143747068]  # شناسه عددی ادمین‌ها
+CHANNEL_TAG = "@hotkose | کُصِ داغ " 
+PING_INTERVAL = 240  # فاصله زمانی بین پینگ ها به ثانیه
