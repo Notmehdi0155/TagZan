a2a818cd6892f3d44017ab47ded64ddd9a403295
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-20 17:36:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = "8035328341:AAHM9URoQiDvXbrUIXsTcLtE7ENzB7qs2zc"
+BOT_TOKEN = "8035328341:AAG2nMlwajk5qX861t_nMt5yqUVx7m1fHzY"
 WEBHOOK_URL = "https://tagzan-1.onrender.com"
 ADMIN_IDS = [6387942633, 7189616405, 5459406429, 7143747068]  # شناسه عددی ادمین‌ها
 CHANNEL_TAG = "@hotkose | کُصِ داغ " 
