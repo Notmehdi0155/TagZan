7ce6b112b895283e723bca961a8ce57fbb3d91f5
Notmehdi0155 <notmehdi0155@gmail.com>
2025-07-21 16:11:54 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 BOT_TOKEN = "8035328341:AAGZ9r9xoTJRXJYBRS5I4QeM8iNfRuEbu8k"
-WEBHOOK_URL = "https://tagzan-1.onrender.com"
+WEBHOOK_URL = "https://tagzan-2.onrender.com"
 ADMIN_IDS = [6387942633, 7189616405, 5459406429, 7143747068]  # شناسه عددی ادمین‌ها
 CHANNEL_TAG = "@hotkose | کُصِ داغ " 
 PING_INTERVAL = 50  # فاصله زمانی بین پینگ ها به ثانیه
