6e6ddb74cfc238590fd9a75160d057f3ac846222
Notmehdi0155 <notmehdi0155@gmail.com>
2025-10-18 01:20:55 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 # config.py
-
-BOT_TOKEN = "8035328341:AAE63qLPYFh-eS3TUSHnQ0Key6V_8a5s7N0"
-WEBHOOK_URL = "https://tagzan-3.onrender.com"
-ADMIN_IDS = [6387942633, 1234567890]  # شناسه عددی ادمین‌ها
-CHANNEL_TAG = "@hotkose | کُصِ داغ " 
-PING_INTERVAL = 30  # فاصله زمانی بین پینگ ها به ثانیه
-PRIVATE_CHANNEL_ID = -1002884014465  # آیدی عددی کانالت
+TOKEN = "توکن ربات خودت را اینجا قرار بده"
+ADMINS = [7189616405, 6387942633, 5459406429]  # آیدی ادمین‌ها
+VIDEO_LINK = "لینک ویدیوی انگیزشی برای پیام پایانی"
